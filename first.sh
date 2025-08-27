969352df2b0700db40cd6162bf87bdc7ef0a0047
srikanth-mallela <113015837+srikanth-mallela@users.noreply.github.com>
2025-08-27 16:16:26 -0500

    first commit

---
+++ first.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "******************First project*****************"
+read -p "enter no.of projects in org:" num
+
+echo $num
+
+touch file1.txt
+
+echo "assigning full permissions"
+
+chmod 755 file1.txt
+
+echo "project completed"
+
+echo "***********thanks for co-ordination*******************"
